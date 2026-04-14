0a70ab80f7c6aea512b7d46e846daaad7e33d3dc
Luca Giampieri <luca.giampieri95@gmail.com>
2026-04-14 16:14:29 +0200

    Spostamento dati in db.php

---
+++ Data/db.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "./Traits/Rateable.php";
+require_once "./Models/Movie.php";
+require_once "./Models/Genre.php";
+
+
+$crime = new Genre("Crime");
+$scifi = new Genre("Sci-fi");
+$thriller = new Genre("Thriller");
+$action = new Genre("Action");
+$drama = new Genre("Drama");
+
+$movies = [
+    $m1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972, [$crime, $drama, $thriller]),
+    $m2 = new Movie("Matrix", "Wachowski", 1999, [$scifi, $action]),
+    $m3 = new Movie("Inception", "Christopher Nolan", 2010, [$scifi, $thriller]),
+    $m4 = new Movie("Parasite", "Bong Joon-ho", 2019, [$thriller, $drama]),
+    $m5 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, [$crime, $drama, $thriller]),
+];
+
+$m1->setRating(10);
+$m3->setRating(8);
+$m4->setRating(9);
